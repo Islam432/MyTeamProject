3b234275ce874e30f48b8022bdf745e50327fbfd
zhandos-manapov <zandosmanapov@gmail.com>
2023-07-06 13:23:07 +0600

    fix: linting and formatting

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,14 +1,67 @@
 module.exports = {
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-  ],
-  parser: '@typescript-eslint/parser',
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': 'warn',
-  },
+    'env': {
+        'browser': true,
+        'es2021': true
+    },
+    'extends': [
+        'eslint:recommended',
+        // 'standard-with-typescript',
+        'plugin:react/recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:react-hooks/recommended',
+        'plugin:prettier/recommended',
+    ],
+    'overrides': [
+        {
+            'env': {
+                'node': true
+            },
+            'files': [
+                '.eslintrc.{js,cjs}'
+            ],
+            'parserOptions': {
+                'sourceType': 'script'
+            }
+        }
+    ],
+    'parser': '@typescript-eslint/parser',
+    'parserOptions': {
+        'ecmaVersion': 'latest',
+        'sourceType': 'module',
+        'parser': '@typescript-eslint/parser',
+        'project': './tsconfig.json'
+    },
+    'plugins': [
+        'react',
+        'react-refresh',
+        '@typescript-eslint',
+        'prettier'
+    ],
+    'rules': {
+      'prettier/prettier': 'error',
+      'quotes': [
+        'error',
+        'single',
+        {
+          'avoidEscape': true
+        }
+      ],
+      'semi': [
+        'error',
+        'never'
+      ],
+      '@typescript-eslint/quotes':[
+        'error',
+        'single',
+        {
+          'avoidEscape': true
+        }
+      ],
+      '@typescript-eslint/semi': [
+        'error',
+        'never'
+      ],
+      'react-refresh/only-export-components': 'warn',
+    },
+    'ignorePatterns': ['.eslintrc.cjs'],
 }
