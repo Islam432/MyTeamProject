bc778cd6e77e7f975b2b1db1af0389b63af4b301
Islam Temirgaliev <islam2004tr@mail.ru>
2023-07-12 09:54:45 +0600

    comit

---
+++ src/modules/authentication/pages/Auth/Auth.tsx
@@ -0,0 +1,13 @@
+
+import Signup from '../../components/Signup/Signup'
+import styles from './Auth.module.scss'
+const Auth = () => {
+  return (
+    <div className={styles.cont}>
+
+        <Signup/>
+    </div>
+  )
+}
+
+export default Auth
