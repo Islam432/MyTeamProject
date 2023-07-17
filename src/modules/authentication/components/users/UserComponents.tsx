b9e09fe7e5e5ea99f7297bac1cb1903035dbe247
Islam Temirgaliev <islam2004tr@mail.ru>
2023-07-17 09:53:35 +0600

    feat:table

--- src/modules/authentication/components/users/UserComponents.tsx
+++ src/modules/authentication/components/users/UserComponents.tsx
@@ -1,40 +1,73 @@
-import * as React from 'react';
-import { DataGrid, GridColDef, GridValueGetterParams } from '@mui/x-data-grid';
-
+import { DataGrid, GridColDef, GridValueGetterParams } from '@mui/x-data-grid'
+import { useState, useEffect } from 'react'
+import { getUsers } from './server'
+import { any, number } from 'zod'
 const columns: GridColDef[] = [
   { field: 'id', headerName: 'ID', width: 70 },
-  { field: 'firstName', headerName: 'First name', width: 130 },
-  { field: 'lastName', headerName: 'Last name', width: 130 },
+  { field: 'first_name', headerName: 'First name', width: 170 },
+  { field: 'last_name', headerName: 'Last name', width: 170 },
   {
-    field: 'age',
-    headerName: 'Age',
+    field: 'email',
+    headerName: 'Email',
+    width: 170,
+  },
+  {
+    field: 'contact_number',
+    headerName: 'Contact_number',
     type: 'number',
-    width: 90,
+    width: 160,
+},
+{
+  field: 'role_name',
+  headerName: 'Role',
+  width: 130,
+},
+  {
+    field: 'date_of_birth',
+    headerName: 'date_of_birth ',
+    width: 170,
   },
   {
-    field: 'fullName',
-    headerName: 'Full name',
-    description: 'This column has a value getter and is not sortable.',
-    sortable: false,
-    width: 160,
-    valueGetter: (params: GridValueGetterParams) =>
-      `${params.row.firstName || ''} ${params.row.lastName || ''}`,
+    field: 'is_active',
+    headerName: 'Status ',
+    width: 170,
   },
-];
+]
 
-const rows = [
-  { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-  { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-  { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },   
-  { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-  { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-  { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-  { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-  { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-  { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
-];
+// const rows = [
+//   { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
+//   { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
+//   { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
+//   { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
+//   { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
+//   { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
+//   { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
+//   { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
+//   { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+// ]
 
 export default function UserComponents() {
+  const [rows, setRows] = useState<any[]>([])
+  useEffect(() => {
+    const xz = async () => {
+      try {
+        const resp: any = await getUsers()
+        console.log(resp.data)
+        const modifiedRows = resp.data.map((row: any) => {
+          return {
+            ...row,
+            date_of_birth: row.date_of_birth.substring(0, 10),
+          }
+        })
+        setRows(modifiedRows)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+
+    xz()
+  }, [])
+
   return (
     <div style={{ height: 500, width: '100%' }}>
       <DataGrid
@@ -49,5 +82,5 @@ export default function UserComponents() {
         checkboxSelection
       />
     </div>
-  );
+  )
 }
