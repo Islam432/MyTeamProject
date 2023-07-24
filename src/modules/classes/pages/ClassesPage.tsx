cfe7e8df403ed61ff7f88b994dec7a9285af19ac
zhandos-manapov <zandosmanapov@gmail.com>
2023-07-24 13:47:31 +0600

    Revert "ghjg"
    This reverts commit 01a1578f1d8bd2ad0b9881a40de4cf2a35ac1600.

--- src/modules/classes/pages/ClassesPage.tsx
+++ src/modules/classes/pages/ClassesPage.tsx
@@ -1,24 +1,5 @@
-import { memo, useState } from 'react'
-import { toggler } from '../class.service';
+import { memo } from 'react'
 
-
-function ClassesPage() {
-  const [isActive, setIsActive] = useState(false);
-
-  const handler = async (event: React.MouseEvent<HTMLButtonElement>) => {
-    event.preventDefault();
-    setIsActive(!isActive);
-    await toggler({ open_for_enrollment: isActive }, 4);  
-  };
-
-  return (
-    <>
-      <h1>Courses</h1>
-      <button onClick={handler} >
-        {isActive ? 'Active' : 'In-Active'}
-      </button>
-    </>
-  );
-}
-
-export default memo(ClassesPage);
+export default memo(function ClassesPage() {
+  return <h1>Courses</h1>
+})
