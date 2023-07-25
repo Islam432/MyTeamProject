c0a731af09c23dd518018451736f78bff284378d
zhandos-manapov <zandosmanapov@gmail.com>
2023-07-25 16:36:33 +0600

    fix: import fix

--- src/modules/dashboard/pages/DashboardPage.tsx
+++ src/modules/dashboard/pages/DashboardPage.tsx
@@ -1,6 +1,5 @@
-// import Dashboard from '../components/Dashboard/Dashboard'
 import { memo } from 'react'
-import Dashboard from '../components/Dashboard/Dashboard'
+import Dashboard from './../components/Dashboard/Dashboard'
 
 export default memo(function DashboardPage() {
   return <Dashboard />
