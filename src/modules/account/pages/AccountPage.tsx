8d8df0d18253225c0d67a5884bc124d533a3cb67
zhandos-manapov <zandosmanapov@gmail.com>
2023-07-25 17:41:29 +0600

    renaming

--- src/modules/account/pages/AccountPage.tsx
+++ src/modules/account/pages/AccountPage.tsx
@@ -1,12 +1,11 @@
-
-import AccountComponents from "../components/AccountComponents/Account";
-
+import Account from '../components/AccountComponents/Account'
 
 import { memo } from 'react'
 
 export default memo(function AccountPage() {
-  return <div>
-  <AccountComponents/>
-</div>
+  return (
+    <div>
+      <Account />
+    </div>
+  )
 })
-
