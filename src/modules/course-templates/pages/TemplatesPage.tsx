dd1a9ce9f5b98358e418c1efe6ba385deb370173
trixtertop <gadelovevgenij4@gmail.com>
2023-07-31 10:44:55 +0600

    fix: edit card and distructed modal

--- src/modules/course-templates/pages/TemplatesPage.tsx
+++ src/modules/course-templates/pages/TemplatesPage.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react'
 import CoursePage from '../components/CoursePage/Course-page'
+// import CoursePage from '../components/CoursePage/Course-page'
 
 export default function TemplatesPage() {
   return <CoursePage />
